e953db0a43e79b03e821f60c2f0e0c5ac1e81e0b
yong-gyun <sygttgudthfr@gmail.com>
2022-11-03 11:43:55 +0900

    no message

--- VR_Defense/Assets/Scripts/Controller/CrabController.cs
+++ VR_Defense/Assets/Scripts/Controller/CrabController.cs
@@ -6,6 +6,13 @@ public class CrabController : MobBase
 {
     private int shildCount = 3;
 
+    protected override void Start()
+    {
+        base.Start();
+
+        Init(50, 8, 3, 5, Define.MobType.Crab);
+    }
+
     public override void OnDamaged(float damage)
     {
         if(shildCount > 0)
--- VR_Defense/Assets/Scripts/Controller/Tower.cs
+++ VR_Defense/Assets/Scripts/Controller/Tower.cs
@@ -6,6 +6,15 @@ public class Tower : MonoBehaviour
 {
     public float HP { get { return _hp; } }
     float _hp = 100;
+    GameObject[] blastLocations = new GameObject[3];
+
+    private void Start()
+    {
+        for(int i = 0; i < blastLocations.Length; i++)
+        {
+            blastLocations[i] = Util.FindChild(gameObject, $"BlastLocation_{i + 1}", true);
+        }
+    }
 
     public void OnDamaged(float damage)
     {
@@ -14,8 +23,29 @@ public class Tower : MonoBehaviour
         Debug.Log($"On damaged tower HP : {_hp}");
         if(_hp <= 0)
         {
-            _hp = 0;
-            Managers.Game.Over();
+            StartCoroutine(OnDie());
         }
+    }
+
+    IEnumerator OnDie()
+    {
+        if (_hp == 0)
+            yield break;
+
+        _hp = 0;
+    
+        for(int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < blastLocations.Length; j++)
+            {
+                GameObject go = Managers.Resource.Instantiate("Effect/ExplosionParticle", blastLocations[j].transform.position, Quaternion.identity);
+                Managers.Resource.Destroy(go, 3f);
+                yield return new WaitForSeconds(1f);
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        Managers.Game.Over();
     }
 }
